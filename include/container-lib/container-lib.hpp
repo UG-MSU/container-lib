16d3e808588b41217c587c6b171444f537d65b75
iyphc <92411130+iyphc@users.noreply.github.com>
2022-11-08 11:37:54 +0300

    Create_sync_cmake (#4)
    * Added subdir and creating library
    
    * Added create_processes and sync functions
    
    * Flow_controlwas renamed to sync
    
    * Added min_req_vers and proj_desc
    
    * The create_proc is now void
    
    * main_proc is also new proc
    
    * added create_proc, changed start and sync

--- include/container-lib/container-lib.hpp
+++ include/container-lib/container-lib.hpp
@@ -1,24 +1,27 @@
 #include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 
 namespace ContainerLib {
-    class Container {
-    public:
-        using time_t = size_t;
-        struct launch_options {
-            time_t time;
-            size_t forks_amount;
-            size_t forks_threshold;
-        };
-
-    private:
-        pid_t main_proc, slave_proc;
-        void main_process(launch_options options) const;
-
-    public:
-        void start(std::string path_to_binary, launch_options options);
-        bool synchronize() const;
+  class Container {
+  public:
+    using time_t = size_t;
+    struct launch_options {
+      time_t time;
+      size_t forks_amount;
+      size_t forks_threshold;
     };
+
+  private:
+    pid_t main_proc, slave_proc;
+    void main_process(launch_options options) const;
+
+  public:
+    void start(std::string path_to_binary, launch_options options);
+    void ContainerLib::Container::create_processes();
+    bool sync() const;
+  };
 }
