38faed665bcaf8da4996b434980075328b0f3441
ippishio <waterlava535@gmail.com>
2022-11-09 01:01:58 +0300

    added pybind to cmakelists & adjusted wrapper.cpp

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -1,6 +1,5 @@
 #include "../extern/pybind11/include/pybind11/pybind11.h"
 #include "../include/container-lib/container-lib.hpp"
-#include<string>
 namespace py = pybind11;
 using namespace std;
 
@@ -13,6 +12,6 @@ PYBIND11_MODULE(container_lib_py, m) {
     .def_readwrite("forks_amount", &ContainerLib::launch_options::forks_amount)
     .def_readwrite("forks_threshold", &ContainerLib::launch_options::forks_threshold);
     m.def("create_processes", &ContainerLib::create_processes, "function that creates processes, returns PID of process");
-    m.def("flow_control", &ContainerLib::flow_control, "bool value of flow control");
+    m.def("sync", &ContainerLib::sync, "sync function");
     m.def("start", &ContainerLib::start, "starts containerization process. Launch options struct: int time - start time, int forks_amount - Amount of sub-processes, int forks_threshold - Maximum amount of sub-processes");
 }
