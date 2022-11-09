81fd452f6be37d589bc8145d4c12ee288e744143
PaRat07 <102048782+PaRat07@users.noreply.github.com>
2022-11-09 11:48:36 +0300

    correct_start (#5)
    * correct_start
    
    * added_create_processes
    
    * Update container-lib.hpp
    
    Co-authored-by: Ilya Savitsky <ipsavitsky234@gmail.com>

--- include/container-lib/container-lib.hpp
+++ include/container-lib/container-lib.hpp
@@ -8,7 +8,8 @@
 
 namespace ContainerLib {
 class Container {
-  public:
+
+public:
     using time_t = size_t;
     struct launch_options {
         time_t time;
@@ -16,13 +17,14 @@ class Container {
         size_t forks_threshold;
     };
 
-  private:
-    pid_t main_proc, slave_proc;
-    void main_process(launch_options options) const;
 
-  public:
-    void start(std::string path_to_binary, launch_options options);
-    void create_processes();
+private:
+    pid_t main_proc, slave_proc;
+    void ptrace_process(launch_options options) const;
+    void create_processes(std::string path_to_binary, std::string args, launch_options options);
+
+public:
+    void start(std::string path_to_binary, launch_options options, std::string args);
     bool sync() const;
 };
-} // namespace ContainerLib
+}
--- src/container-lib.cpp
+++ src/container-lib.cpp
@@ -1,7 +1,7 @@
+#include <iostream>
 #include "container-lib/container-lib.hpp"
 
-namespace ContainerLib {
-void Container::main_process(launch_options) const {
+void ContainerLib::Container::ptrace_process(launch_options options) const {
     int status;
     do {
         ptrace(PTRACE_SYSCALL, slave_proc, nullptr, nullptr);
@@ -10,31 +10,31 @@ void Container::main_process(launch_options) const {
     } while (WIFEXITED(status));
 }
 
-void Container::create_processes() {
+void ContainerLib::Container::start(std::string path_to_binary,
+                                    launch_options options, std::string args) {
     main_proc = fork();
-    if (getpid() != main_proc)
-        slave_proc = fork();
-    return;
+    if (main_proc != 0) {
+        return;
+    } else {
+        create_processes(std::move(path_to_binary), std::move(args),
+                         std::move(options));
+    }
 }
 
-void Container::start(std::string path_to_binary, launch_options options) {
-    create_processes();
-    if (getpid() != main_proc && getpid() != slave_proc)
-        main_process(options);
-    else {
+bool ContainerLib::Container::sync() const {
+    return waitpid(main_proc, nullptr, 0);
+}
+
+void ContainerLib::Container::create_processes(
+    std::string path_to_binary, std::string args,
+    ContainerLib::Container::launch_options options) {
+    int streams[2];
+    slave_proc = fork();
+    if (slave_proc != 0) {
+        ptrace_process(options);
+    } else {
         ptrace(PTRACE_TRACEME, 0, 0, 0);
-        execl(path_to_binary.data(), nullptr);
+        execl(path_to_binary.data(), args.data(), nullptr);
         perror("execl");
     }
 }
-
-bool Container::sync() const {
-    if (getpid() != main_proc && getpid() != slave_proc) {
-        wait(nullptr);
-        wait(nullptr);
-    }
-    return 1; // the stub, have to change
-}
-
-} // namespace ContainerLib
-
