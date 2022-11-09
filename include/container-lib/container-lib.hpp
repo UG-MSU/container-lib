02e8e2d18d2e3aaa94acf88fa1b97e3164a354ab
parat07 <pasha.at.phone@gmail.com>
2022-11-09 11:38:32 +0300

    added_create_processes

--- include/container-lib/container-lib.hpp
+++ include/container-lib/container-lib.hpp
@@ -5,20 +5,23 @@
 
 namespace ContainerLib {
     class Container {
-    public:
-        using time_t = size_t;
-        struct launch_options {
-            time_t time;
-            size_t forks_amount;
-            size_t forks_threshold;
-        };
-
-    private:
-        pid_t main_proc, slave_proc;
-        void main_process(launch_options options) const;
-
-    public:
-        void start(std::string path_to_binary, launch_options options, std::string args);
-        bool sync() const;
+  public:
+    using time_t = size_t;
+    struct launch_options {
+        time_t time;
+        size_t forks_amount;
+        size_t forks_threshold;
     };
+
+  private:
+    pid_t main_proc, slave_proc;
+    void ptrace_process(launch_options options) const;
+    void create_processes(std::string path_to_binary, std::string args,
+                          launch_options options);
+
+  public:
+    void start(std::string path_to_binary, launch_options options,
+               std::string args);
+    bool sync() const;
+};
 }
--- src/container-lib.cpp
+++ src/container-lib.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
 #include "container-lib/container-lib.hpp"
 
-void ContainerLib::Container::main_process(launch_options options) const {
+void ContainerLib::Container::ptrace_process(launch_options options) const {
     int status;
     do {
         ptrace(PTRACE_SYSCALL, slave_proc, nullptr, nullptr);
         waitpid(slave_proc, &status, 0);
         std::cout << status << std::endl;
-    }
-    while (WIFEXITED(status));
+    } while (WIFEXITED(status));
 }
 
-
-void ContainerLib::Container::start(std::string path_to_binary, launch_options options, std::string args) {
+void ContainerLib::Container::start(std::string path_to_binary,
+                                    launch_options options, std::string args) {
     main_proc = fork();
-    if (main_proc) {
+    if (main_proc != 0) {
         return;
-    }
-    else {
-        slave_proc = fork();
-        if (slave_proc) {
-            main_process(options);
-        }
-        else {
-            ptrace(PTRACE_TRACEME, 0, 0, 0);
-            execl(path_to_binary.data(), args.data());
-            perror("execl");
-        }
+    } else {
+        create_processes(std::move(path_to_binary), std::move(args),
+                         std::move(options));
     }
 }
 
 bool ContainerLib::Container::sync() const {
     return waitpid(main_proc, nullptr, 0);
+}
+
+void ContainerLib::Container::create_processes(
+    std::string path_to_binary, std::string args,
+    ContainerLib::Container::launch_options options) {
+    int streams[2];
+    slave_proc = fork();
+    if (slave_proc != 0) {
+        ptrace_process(options);
+    } else {
+        ptrace(PTRACE_TRACEME, 0, 0, 0);
+        execl(path_to_binary.data(), args.data(), nullptr);
+        perror("execl");
+    }
 }
