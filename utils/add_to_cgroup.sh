c38168d7d41cc4c36e006b8ed7b2dd915e2b726d
ippishio <waterlava535@gmail.com>
2022-11-11 18:03:06 +0300

    added newlines

--- utils/add_to_cgroup.sh
+++ utils/add_to_cgroup.sh
@@ -3,3 +3,4 @@ pidt=$1
 GROUP_ID=$2
 GROUP_PATH="/sys/fs/cgroup"
 echo "$pidt" > "$GROUP_PATH/$GROUP_ID/cgroup.procs"
+
--- utils/deinit_cgroup.sh
+++ utils/deinit_cgroup.sh
@@ -4,3 +4,4 @@ GROUP_ID=$1
 GROUP_PATH="/sys/fs/cgroup"
 umount "$GROUP_PATH/$GROUP_ID/"
 rmdir "$GROUP_PATH/$GROUP_ID/"
+
