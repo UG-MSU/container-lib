0d49e12a65787efc70c6c1b517b5e0481c4ead53
Clang Format <clang-format@karaman.dev>
2022-11-11 17:32:47 +0000

    apply clang-format

--- include/container-lib/container-lib.hpp
+++ include/container-lib/container-lib.hpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <string>
 #include <sstream>
 #include <stdlib.h>
-#include <sys/user.h>
+#include <string>
 #include <sys/ptrace.h>
 #include <sys/types.h>
-
+#include <sys/user.h>
 
 namespace ContainerLib {
 class Container {
@@ -18,7 +17,6 @@ class Container {
         size_t forks_threshold;
     };
 
-
   private:
     pid_t main_proc, slave_proc;
     fd_t ptrace2exec[2], exec2ptrace[2], ptrace2main[2];
@@ -27,7 +25,7 @@ class Container {
     void create_processes(std::string path_to_binary, std::string args,
                           launch_options options);
     void pipe_init();
-    void get_output(fd_t * fd);
+    void get_output(fd_t *fd);
 
   public:
     void start(std::string path_to_binary, launch_options options,
--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -5,17 +5,21 @@ using namespace std;
 using namespace ContainerLib;
 
 PYBIND11_MODULE(container_lib_py, m) {
-    m.doc() = "containerization python lib"; 
+    m.doc() = "containerization python lib";
     py::class_<Container>(m, "Container")
-    .def(py::init<>())
-    .def("create_processes", &Container::create_processes, "function that creates processe")
-    .def("sync", &Container::sync, "sync function")
-    .def("start", &Container::start, "starts containerization process. Launch options struct: int time - start time, int forks_amount - Amount of sub-processes, int forks_threshold - Maximum amount of sub-processes");
-    py::class_<Container::launch_options>(m, "launch_options",py::dynamic_attr())
-    .def(py::init<>())
-    .def_readwrite("time", &Container::launch_options::time)
-    .def_readwrite("forks_amount", &Container::launch_options::forks_amount)
-    .def_readwrite("forks_threshold", &Container::launch_options::forks_threshold);
-    
+        .def(py::init<>())
+        .def("create_processes", &Container::create_processes,
+             "function that creates processe")
+        .def("sync", &Container::sync, "sync function")
+        .def("start", &Container::start,
+             "starts containerization process. Launch options struct: int time "
+             "- start time, int forks_amount - Amount of sub-processes, int "
+             "forks_threshold - Maximum amount of sub-processes");
+    py::class_<Container::launch_options>(m, "launch_options",
+                                          py::dynamic_attr())
+        .def(py::init<>())
+        .def_readwrite("time", &Container::launch_options::time)
+        .def_readwrite("forks_amount", &Container::launch_options::forks_amount)
+        .def_readwrite("forks_threshold",
+                       &Container::launch_options::forks_threshold);
 }
-
