68dc605b386e67829f006164529e977fe5f453e9
Ilya Timofeev <waterlava535@gmail.com>
2022-11-11 20:56:52 +0300

    Cgroups (#16)
    * added python bind for lib
    
    * added python bind for lib
    
    * added gitignore
    
    * Merge branch 'start_and_cmake' of https://github.com/UG-MSU/container-lib into wrapper
    
    * added pybind to cmakelists & adjusted wrapper.cpp
    
    * added class support
    
    * added newlines
    
    * really added newlines
    
    * really added newlines
    
    * added cgroups functions
    
    * added newlines
    
    * formatted files && deleted build.sh

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -1,6 +1,5 @@
 #include "../extern/pybind11/include/pybind11/pybind11.h"
-#include "container-lib/container-lib.hpp"
-namespace py = pybind11;
+#include "container-lib/container-lib.hpp" namespace py = pybind11;
 using namespace std;
 using namespace ContainerLib;
 
