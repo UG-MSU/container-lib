01ec2b6f3c84876e58907bcd394517f8bcaa34d4
ippishio <waterlava535@gmail.com>
2022-11-12 22:02:46 +0300

    fixed all

--- src/cgroups.c
+++ src/cgroups.c
@@ -1,24 +1,25 @@
-#include<sys/types.h>
-#include<sys/wait.h>
-#include <sys/mount.h>
 #include <errno.h>
-#include<stdio.h>
-#include <stdlib.h>
 #include <sstream>
-#include<unistd.h>
-void init_cgroup(int _MEM_SIZE, float _TOTAL_CPU_PERCENTAGE, char CGROUP_ID[20]) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mount.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+void init_cgroup(int _MEM_SIZE, float _TOTAL_CPU_PERCENTAGE,
+                 char CGROUP_ID[20]) {
     std::stringstream ss;
-    ss << "../utils/init_cgroup.sh " << _MEM_SIZE << " " << _TOTAL_CPU_PERCENTAGE << " " << CGROUP_ID;
-        system(ss.str().c_str());
-    }
+    ss << "../utils/init_cgroup.sh " << _MEM_SIZE << " "
+       << _TOTAL_CPU_PERCENTAGE << " " << CGROUP_ID;
+    system(ss.str().c_str());
+}
 void add_to_cgroup(pid_t pid, char CGROUP_ID[20]) {
     std::stringstream ss;
     ss << "../utils/add_to_cgroup.sh " << pid << " " << CGROUP_ID;
-        system(ss.str().c_str());
+    system(ss.str().c_str());
 }
 void deinit_cgroup(char CGROUP_ID[20]) {
     std::stringstream ss;
     ss << "../utils/deinit_cgroup.sh " << CGROUP_ID;
-        system(ss.str().c_str());
+    system(ss.str().c_str());
 }
-
