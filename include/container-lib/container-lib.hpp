044bc2097317b464509eff4b7f6fc2f93e01c874
PaRat07 <102048782+PaRat07@users.noreply.github.com>
2022-11-18 17:20:24 +0300

    added_processing_of_clone_and_exec

--- include/container-lib/container-lib.hpp
+++ include/container-lib/container-lib.hpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <sstream>
 #include <stdlib.h>
 #include <string>
 #include <sys/ptrace.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <sys/user.h>
 
 namespace ContainerLib {
 class Container {
   public:
     using time_t = size_t;
+    using fd_t = int;
     struct launch_options {
         time_t time;
         size_t forks_amount;
@@ -18,15 +19,15 @@ class Container {
 
   private:
     pid_t main_proc, slave_proc;
-    int ptrace2exec[2], exec2ptrace[2];
-    void ptrace_process(launch_options options) const;
-    void create_processes(std::string path_to_binary, std::string args,
-                          launch_options options);
+    fd_t ptrace2exec[2], exec2ptrace[2], ptrace2main[2];
+    std::string buf;
+    void ptrace_process(launch_options options);
+    void create_processes(std::string path_to_binary, std::string args, launch_options options);
     void pipe_init();
+    void get_output(fd_t *fd);
 
   public:
-    void start(std::string path_to_binary, launch_options options,
-               std::string args);
-    bool sync() const;
+    void start(std::string path_to_binary, launch_options options, std::string args);
+    bool sync();
 };
 } // namespace ContainerLib
--- src/container-lib.cpp
+++ src/container-lib.cpp
@@ -1,6 +1,5 @@
 #include "container-lib/container-lib.hpp"
 #include <asm/unistd.h>
-#include <sys/user.h>
 #include <sys/wait.h>
 
 void ContainerLib::Container::ptrace_process(launch_options options) {
@@ -18,28 +17,27 @@ void ContainerLib::Container::ptrace_process(launch_options options) {
         // at syscall
         if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80) {
             ptrace(PTRACE_GETREGS, slave_proc, 0, &state);
+
             switch (state.orig_rax) {
-            case __NR_clone:
-                if (fork() == 0) {
-                    ptrace(PTRACE_GETREGS, slave_proc, 0, &state);
-                    slave_proc = state.rax;
-                    ptrace(PTRACE_ATTACH, slave_proc, 0, 0);
-                    waitpid(slave_proc, &status, 0);
-                    ptrace(PTRACE_SETOPTIONS, slave_proc, 0, PTRACE_O_TRACESYSGOOD);
-                }
-                break;
             case __NR_execve:
+                std::cout << "process " << slave_proc << " tried to execute a binary! killing process!" << std::endl;
                 state.rax = __NR_kill;
                 state.rdi = slave_proc;
                 state.rsi = SIGKILL;
                 ptrace(PTRACE_SETREGS, slave_proc, 0, &state);
                 ptrace(PTRACE_CONT, slave_proc, 0, 0);
                 waitpid(slave_proc, nullptr, 0);
-                std::cout << "Program broken because it called execv" << std::endl;
                 return;
+            case __NR_clone:
+                std::cout << "process " << slave_proc << " tried to clone itself! killing process!" << std::endl;
+                state.rax = __NR_kill;
+                state.rdi = slave_proc;
+                state.rsi = SIGKILL;
+                ptrace(PTRACE_SETREGS, slave_proc, 0, &state);
+                ptrace(PTRACE_CONT, slave_proc, 0, 0);
+                waitpid(slave_proc, nullptr, 0);
             default:
                 std::cout << "SYSCALL " << state.orig_rax << " at " << state.rip << std::endl;
-                break;
             }
 
             // skip after syscall
@@ -51,6 +49,7 @@ void ContainerLib::Container::ptrace_process(launch_options options) {
 
 void ContainerLib::Container::start(std::string path_to_binary,
                                     launch_options options, std::string args) {
+    pipe_init();
     main_proc = fork();
     if (main_proc != 0) {
         return;
@@ -59,8 +58,15 @@ void ContainerLib::Container::start(std::string path_to_binary,
     }
 }
 
-bool ContainerLib::Container::sync() const {
-    return waitpid(main_proc, nullptr, 0);
+bool ContainerLib::Container::sync() {
+    int status;
+    if (slave_proc != 0 && main_proc != 0)
+        wait(&status);
+    if (WIFEXITED(status)) {
+        get_output(ptrace2main);
+        return 1;
+    }
+    return 0;
 }
 
 void ContainerLib::Container::create_processes(
@@ -71,7 +77,24 @@ void ContainerLib::Container::create_processes(
         ptrace_process(options);
     } else {
         ptrace(PTRACE_TRACEME, 0, 0, 0);
+        dup2(ptrace2exec[0], STDIN_FILENO);
+        dup2(exec2ptrace[1], STDOUT_FILENO);
         execl(path_to_binary.data(), args.data(), nullptr);
         perror("execl");
     }
+}
+
+void ContainerLib::Container::pipe_init() {
+    pipe(ptrace2exec);
+    pipe(exec2ptrace);
+    pipe(ptrace2main);
+}
+
+void ContainerLib::Container::get_output(fd_t *fd) { // updates buf
+    std::stringstream input;
+    char tmp;
+    while (read(fd[0], &tmp, sizeof(char)) != 0) {
+        input << tmp;
+    }
+    buf = input.str();
 }
