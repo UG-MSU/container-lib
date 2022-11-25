4c28ff7387e9f8af43379c8249c4bf1d7a23ca5b
iyphc <werhistudy@yandex.ru>
2022-11-25 22:08:39 +0300

    updated pipes

--- include/container-lib/container-lib.hpp
+++ include/container-lib/container-lib.hpp
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <iostream>
 #include <sstream>
 #include <stdlib.h>
@@ -7,6 +8,15 @@
 #include <sys/user.h>
 
 namespace ContainerLib {
+
+#define SAFE(func, call)                                                       \
+    {                                                                          \
+        if ((call) < 0) {                                                      \
+            perror(func);                                                      \
+            exit(1);                                                           \
+        }                                                                      \
+    }
+
 class Container {
   public:
     using time_t = size_t;
@@ -15,17 +25,20 @@ class Container {
         time_t time;
         size_t forks_amount;
         size_t forks_threshold;
-    };
+        std::string input;
+    } options;
 
   private:
-    pid_t main_proc, slave_proc;
+    pid_t ptrace_proc, slave_proc;
     fd_t ptrace2exec[2], exec2ptrace[2], ptrace2main[2];
     std::string buf;
+
     void ptrace_process(launch_options options);
     void create_processes(std::string path_to_binary, std::string args,
                           launch_options options);
     void pipe_init();
-    void get_output(fd_t *fd);
+    void get_output(const fd_t *fd);
+    void write_to_fd(const fd_t *fd, const char *string, size_t size);
 
   public:
     void start(std::string path_to_binary, launch_options options,
