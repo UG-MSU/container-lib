3b26a0622b4b2b3a8bd7b950205748dbd3891538
iyphc <werhistudy@yandex.ru>
2022-11-25 22:10:15 +0300

    Added dup and updated pipes

--- src/container-lib.cpp
+++ src/container-lib.cpp
@@ -1,20 +1,23 @@
 #include "container-lib/container-lib.hpp"
 #include <asm/unistd.h>
+#include <cstring>
 #include <sys/user.h>
 #include <sys/wait.h>
 
 void ContainerLib::Container::ptrace_process(launch_options options) {
-    int status;
+    int status, exit_status;
+    ;
     waitpid(slave_proc, &status, 0);
 
     ptrace(PTRACE_SETOPTIONS, slave_proc, 0, PTRACE_O_TRACESYSGOOD);
+
     while (!WIFEXITED(status)) {
 
         user_regs_struct state{};
 
         ptrace(PTRACE_SYSCALL, slave_proc, 0, 0);
         waitpid(slave_proc, &status, 0);
-
+        
         // at syscall
         if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80) {
             ptrace(PTRACE_GETREGS, slave_proc, 0, &state);
@@ -50,13 +53,16 @@ void ContainerLib::Container::ptrace_process(launch_options options) {
             waitpid(slave_proc, &status, 0);
         }
     }
+    get_output(exec2ptrace);
+    write_to_fd(ptrace2main, buf.c_str(), buf.size());
+    exit(exit_status);
 }
 
 void ContainerLib::Container::start(std::string path_to_binary,
                                     launch_options options, std::string args) {
     pipe_init();
-    main_proc = fork();
-    if (main_proc != 0) {
+    ptrace_proc = fork();
+    if (ptrace_proc != 0) {
         return;
     } else {
         create_processes(std::move(path_to_binary), std::move(args), options);
@@ -64,9 +70,10 @@ void ContainerLib::Container::start(std::string path_to_binary,
 }
 
 bool ContainerLib::Container::sync() {
+    int a = getpid();
     int status;
-    if (slave_proc != 0 && main_proc != 0)
-        wait(&status);
+    if (slave_proc != 0 && ptrace_proc != 0)
+        waitpid(ptrace_proc, &status, 0);
     if (WIFEXITED(status)) {
         get_output(ptrace2main);
         return 1;
@@ -82,6 +89,7 @@ void ContainerLib::Container::create_processes(
         ptrace_process(options);
     } else {
         ptrace(PTRACE_TRACEME, 0, 0, 0);
+        write_to_fd(ptrace2exec, options.input.c_str(), options.input.size());
         dup2(ptrace2exec[0], STDIN_FILENO);
         dup2(exec2ptrace[1], STDOUT_FILENO);
         execl(path_to_binary.data(), args.data(), nullptr);
@@ -93,15 +101,30 @@ void ContainerLib::Container::pipe_init() {
     pipe(ptrace2exec);
     pipe(exec2ptrace);
     pipe(ptrace2main);
+    fcntl(exec2ptrace[0], F_SETFL, O_NONBLOCK);
+    fcntl(ptrace2main[0], F_SETFL, O_NONBLOCK);
+    fcntl(ptrace2exec[0], F_SETFL, O_NONBLOCK);
 }
 
 std::string ContainerLib::Container::get_buf() const { return buf; }
 
-void ContainerLib::Container::get_output(fd_t *fd) { // updates buf
+void ContainerLib::Container::get_output(const fd_t *fd) { // updates buf
     std::stringstream input;
     char tmp;
-    while (read(fd[0], &tmp, sizeof(char)) != 0) {
+    int nread = 0;
+    while (nread != -1) {
+        nread = read(fd[0], &tmp, 1);
         input << tmp;
     }
     buf = input.str();
+    return;
+}
+
+void ContainerLib::Container::write_to_fd(const fd_t *fd, const char *string,
+                                          size_t size) {
+    char *tmp = new char[size];
+    strcpy(tmp, string);
+    int write_status = write(fd[1], tmp, size);
+    delete[] tmp;
+    return;
 }
