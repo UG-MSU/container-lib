3165373cbfc9cfd30f923fcea0aff6405a13736e
PaRat07 <102048782+PaRat07@users.noreply.github.com>
2022-11-25 17:31:53 +0300

    added_correct_runtime_error

--- src/container-lib.cpp
+++ src/container-lib.cpp
@@ -16,6 +16,7 @@ void ContainerLib::Container::ptrace_process(launch_options options) const {
         // at syscall
         if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80) {
             ptrace(PTRACE_GETREGS, slave_proc, 0, &state);
+            exit_status return_status;
             switch (state.orig_rax) {
             case __NR_execve:
                 std::cout << "process " << slave_proc << " tried to execute a binary! killing process!" << std::endl;
@@ -25,7 +26,9 @@ void ContainerLib::Container::ptrace_process(launch_options options) const {
                 ptrace(PTRACE_SETREGS, slave_proc, 0, &state);
                 ptrace(PTRACE_CONT, slave_proc, 0, 0);
                 waitpid(slave_proc, nullptr, 0);
-                exit(1);
+                return_status = exit_status::run_time_error;
+                write(pipe_for_exit_status[1], &return_status, sizeof(exit_status));
+                return;
             case __NR_clone:
                 std::cout << "process " << slave_proc << " tried to clone itself! killing process!" << std::endl;
                 state.rax = __NR_kill;
@@ -33,8 +36,9 @@ void ContainerLib::Container::ptrace_process(launch_options options) const {
                 state.rsi = SIGKILL;
                 ptrace(PTRACE_SETREGS, slave_proc, 0, &state);
                 ptrace(PTRACE_CONT, slave_proc, 0, 0);
-                waitpid(slave_proc, nullptr, 0);
-                exit(1);
+                return_status = exit_status::run_time_error;
+                write(pipe_for_exit_status[1], &return_status, sizeof(exit_status));
+                return;
             default:
                 std::cout << "SYSCALL " << state.orig_rax << " at " << state.rip << std::endl;
             }
@@ -44,21 +48,28 @@ void ContainerLib::Container::ptrace_process(launch_options options) const {
             waitpid(slave_proc, &status, 0);
         }
     }
+    exit_status return_status = exit_status::ok;
+    write(pipe_for_exit_status[1], &return_status, sizeof(exit_status));
 }
 
 void ContainerLib::Container::start(std::string path_to_binary, launch_options options, std::string args) {
     pipe_init();
     main_proc = fork();
     if (main_proc != 0) {
+        close(pipe_for_exit_status[1]);
         return;
     } else {
+        close(pipe_for_exit_status[0]);
         create_processes(std::move(path_to_binary), std::move(args), options);
     }
 }
 
 ContainerLib::Container::exit_status ContainerLib::Container::sync() const {
     waitpid(main_proc, nullptr, 0);
-    return exit_status::ok;
+    exit_status status;
+    read(pipe_for_exit_status[0], &status, sizeof(exit_status));
+
+    return status;
 }
 
 void ContainerLib::Container::create_processes(
@@ -79,6 +90,7 @@ void ContainerLib::Container::create_processes(
 void ContainerLib::Container::pipe_init() {
     pipe(ptrace2exec);
     pipe(exec2ptrace);
+    pipe(pipe_for_exit_status);
 }
 
 std::string ContainerLib::Container::get_buf() const { 
