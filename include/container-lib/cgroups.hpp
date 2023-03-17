e3615d22c43f7149c031416973342632936c71ed
CapybaraV <yskiwolf01@rambler.ru>
2023-03-17 17:59:36 +0300

    fixed and tested exception

--- include/container-lib/cgroups.hpp
+++ include/container-lib/cgroups.hpp
@@ -1,5 +1,6 @@
 #ifndef CGROUPS_H
 #define CGROUPS_H
+#include <container-lib/exception.hpp>
 #include <errno.h>
 #include <fcntl.h>
 #include <fstream>
@@ -16,7 +17,6 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
-#include <container-lib/exception.hpp>
 
 void echo_to_file(std::string path, std::string text);
 int cgroup_verison(std::string CGROUP_PATH);
@@ -26,10 +26,10 @@ void add_to_cgroup(pid_t pid, std::string CGROUP_ID);
 void deinit_cgroup(std::string CGROUP_ID);
 #define FILE_EXISTS(file) access((file).c_str(), F_OK) == 0
 #define SAFE(func, call)                                                       \
-    if ((call) < 0) {                                                            \
-        throw ContainerLib::Exception(func);                                                        \
-    }                                                                                   
-#define intstr(s) std::to_string(s)                                                     
+    if ((call) < 0) {                                                          \
+        throw ContainerLib::Exception(func);                                   \
+    }
+#define intstr(s) std::to_string(s)
 
 const int64_t CGROUPV2_MAGIC = 1667723888;
 const int64_t CGROUPV1_MAGIC = 2613483;
--- src/exception.cpp
+++ src/exception.cpp
@@ -1,6 +1,7 @@
-// perror - проверка rno и exception
 #include "container-lib/exception.hpp"
+
 using namespace ContainerLib;
-const char * Exception::what() const throw() {
-    return Exception::text;
+
+const char *Exception::what() const throw() { 
+    return Exception::text; 
 }
