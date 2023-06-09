c1f11f31144e6a34dca46d3910a14c3e6b5c49d2
CapybaraV <yskiwolf01@rambler.ru>
2023-06-09 14:37:21 +0300

    stash

--- src/interpreted-test.cpp
+++ src/interpreted-test.cpp
@@ -2,5 +2,5 @@
 
 ContainerLib::ContainerDocker::interpreted_test::interpreted_test(
     int port, std::string dockerfile_path) {
-    ContainerLib::ContainerDocker::client::client(port);
+    // ContainerLib::ContainerDocker::client::client(port);
 }
